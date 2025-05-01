731e109da40c29118f4655c4547c92124401197d
Eunjin <eunjinlee0128@gmail.com>
2025-05-01 16:17:25 +0900

    🔒️ API 키 gitignore파일로 이동

---
+++ .gitignore/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC0UyuacvbSYQmYFMcasvhz_2CYDzvXGt8",
+  authDomain: "gearquest-99781.firebaseapp.com",
+  projectId: "gearquest-99781",
+  storageBucket: "gearquest-99781.firebasestorage.app",
+  messagingSenderId: "277657155200",
+  appId: "1:277657155200:web:233a25497feaf425a7d865",
+  measurementId: "G-6DPT370PZH"
+};
--- pages/firebase.js
+++ pages/firebase.js
@@ -3,20 +3,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/fireba
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-storage.js";
+import firebaseConfig from "../.gitignore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyC0UyuacvbSYQmYFMcasvhz_2CYDzvXGt8",
-  authDomain: "gearquest-99781.firebaseapp.com",
-  projectId: "gearquest-99781",
-  storageBucket: "gearquest-99781.firebasestorage.app",
-  messagingSenderId: "277657155200",
-  appId: "1:277657155200:web:233a25497feaf425a7d865",
-  measurementId: "G-6DPT370PZH"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
