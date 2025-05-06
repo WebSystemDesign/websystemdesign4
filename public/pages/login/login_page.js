981c7df94924df8083a1846279037e45d9e7a210
정수민 <jsm24@sookmyung.ac.kr>
2025-05-06 18:50:55 +0900

    🎨로그인컨테이너추가2

--- public/pages/login/login_page.js
+++ public/pages/login/login_page.js
@@ -1,11 +1,15 @@
 class Login {
     template() {
         return `
-            <h1>로그인</h1>
-            <input type="email" id="email" placeholder="이메일 입력"><br>
-            <input type="password" id="password" placeholder="비밀번호 입력"><br>
-            <button id="loginBtn">로그인</button>
-            <p id="errorMsg" style="color:red"></p>
+            <div class = "login-container">
+                <div class = "login-inner-container">
+                    <h1 id="login-text">로그인</h1>
+                    <input type="email" id="email" placeholder="이메일 입력"><br>
+                    <input type="password" id="password" placeholder="비밀번호 입력"><br>
+                    <button id="loginBtn">로그인</button>
+                    <p id="errorMsg" style="color:red"></p>
+                </div>
+            </div>
         `;
     }
 }
