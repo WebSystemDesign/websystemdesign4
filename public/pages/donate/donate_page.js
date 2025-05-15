df02f35a162089cec011758763902a77659dfa19
정수민 <jsm24@sookmyung.ac.kr>
2025-05-15 20:03:14 +0900

    ✨ 기부 페이지 틀 작성

---
+++ public/pages/donate/donate_page.js
@@ -0,0 +1,15 @@
+class Donate {
+    template() {
+        return `
+            <div class="wallpaper-container">
+                <h1 class="donate-text">Donate</h1>
+                <div class="donate-content">
+                    <img id= "donate-profile" src = "" alt="profile-img">
+                    <p>테스트</p>
+                </div>
+            </div>
+        `;
+    }
+}
+
+export default new Donate();
