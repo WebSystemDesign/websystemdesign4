b50075a00c3e22d46822ca70d59346565170bc19
Entity08 <eunjinlee0128@gmail.com>
2025-05-27 12:23:13 +0900

    ✨ 도시별 매장 리스트 구현

--- public/pages/offline/offline_page.js
+++ public/pages/offline/offline_page.js
@@ -1,10 +1,16 @@
 import { waitForNaverMapAndInit } from "./map.js";
+import { loadStoreAndDisplayCity } from "./city.js";
 
 class Offline {
     template() {
         return `
             <div class="wallpaper-container">
                 <div id="map" style="width:100%; height:500px;"></div>
+                <div class="city-title">
+                    <br>
+                    <h2>지역별 매장 안내</h2>
+                    <br>
+                </div>
                 <div id="city"></div>
             </div>
         `;
@@ -12,6 +18,7 @@ class Offline {
 
     mounted() {
         waitForNaverMapAndInit();
+        loadStoreAndDisplayCity();
     }
 }
 
