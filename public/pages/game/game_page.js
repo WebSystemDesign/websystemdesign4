8bedec47d50b9ea85b7593db9d6ad112aaed5e64
Eunjin <eunjinlee0128@gmail.com>
2025-05-29 17:46:56 +0900

    💄 악세사리, 콘솔 검색창 수정

--- public/pages/game/game_page.js
+++ public/pages/game/game_page.js
@@ -2,23 +2,23 @@ class Game {
     template() {
         return `
             <div class="game-page">
-            <div class="game-header">
-            <div class="game-search-container">
-            <form class="game-search-box">
-                <img class="game-search-logo" src="../sources/logo_icon.png">
-                <input class="game-search-text" type="text" placeholder="게임을 검색하세요.">
-                <button class="game-search-btn" type="submit">
-                    <img src="../sources/search.png" alt="검색" />
-                </button>
-            </form>
-            </div>
-            <button class="game-submit" id="game-submit" type="button">선택 완료</button>
-            </div>
+                <div class="game-header">
+                    <div class="game-search-container">
+                    <form class="game-search-box">
+                        <img class="game-search-logo" src="../sources/logo_icon.png">
+                        <input class="game-search-text" type="text" placeholder="게임을 검색하세요.">
+                        <button class="game-search-btn" type="submit">
+                            <img src="../sources/search.png" alt="검색" />
+                        </button>
+                    </form>
+                    </div>
+                <button class="game-submit" id="game-submit" type="button">선택 완료</button>
+                </div>
 
-            <div class="game-card-list">
-                <div id="game-container"></div>
+                <div class="game-card-list">
+                    <div id="game-container"></div>
+                </div>
             </div>
-        </div>
         `;
     }
 }
